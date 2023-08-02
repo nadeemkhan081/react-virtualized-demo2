fe4d568f834b4893fc92d3960049357a00d63995
MOHAMMAD NADEEM <mnadeem.mn838@gmail.com>
2023-08-02 23:10:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Table, Column, AutoSizer } from 'react-virtualized';
+import 'react-virtualized/styles.css'; 
 
-function App() {
+import faker from 'faker';
+
+const generateRandomData = (count) => {
+  const data = [];
+  for (let i = 0; i < count; i++) {
+    const item = {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      age: Math.floor(Math.random() * 40) + 20, 
+    };
+    data.push(item);
+  }
+  return data;
+};
+
+const App = () => {
+  const rowCount = 10000; // Number of rows to generate
+  const rowHeight = 50; 
+
+  const data = generateRandomData(rowCount);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '500px', width: '100%' }}>
+      <AutoSizer>
+        {({ height, width }) => (
+          <Table
+            width={width}
+            height={height}
+            headerHeight={rowHeight}
+            rowHeight={rowHeight}
+            rowCount={rowCount}
+            rowGetter={({ index }) => data[index]}
+          >
+            <Column dataKey="name" label="Name" width={200} />
+            <Column dataKey="email" label="Email" width={300} />
+            <Column dataKey="age" label="Age" width={100} />
+          </Table>
+        )}
+      </AutoSizer>
     </div>
   );
-}
+};
 
 export default App;
